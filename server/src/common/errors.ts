ee05af10eb61648da435ecacddd28a45055fcb58
Pratik Jussal <pratikjaslofficial@gmail.com>
2025-03-12 15:17:54 +0530

    User Data Validation Middleware Added

---
+++ server/src/common/errors.ts
@@ -0,0 +1,11 @@
+export const ERROR_MESSAGES = {
+    // INVALID_GENDER: 'Gender must be one of [male, female, other]',
+    MISSING_FIELD: (field: string) => `${field} is required.`,
+    VALIDATION_FAILED: "Validation Failed",
+    SERVER_ERROR: 'Something went wrong, please try again later.',
+    INVALID_EMAIL: 'Email format is invalid.',
+    USER_ALREADY_EXISTS: 'A user with this email already exists.',
+    USER_NOT_FOUND: 'User not found.',
+    INVALID_AGE: 'Age must be greater than 0.',
+    INVALID_PASSWORD: 'Password must meet the required criteria.',
+  };
---
+++ server/src/middlewares/validateUserData.ts
@@ -0,0 +1,36 @@
+import { Request, Response, NextFunction } from "express";
+import { userSchema } from "../schema/userSchema";
+import { ERROR_MESSAGES } from "../common/errors";
+
+export async function validateUserData(req: Request, res: Response, next: NextFunction): Promise<void>{
+    const {
+        fullName,
+        age,
+        gender,
+        email,
+        phone,
+        password,
+        role,
+        joinDate
+    } = req.body;
+
+    try {
+        if(!fullName || !age || !gender || !email || !phone || !password || !role || !joinDate){
+            res.status(400).json({ success: "false", message: ERROR_MESSAGES.MISSING_FIELD });
+            return
+        }
+
+        const {error, value} = await userSchema.validateAsync(req.body);
+
+        if(error){
+            res.status(400).json({success: "false", message: ERROR_MESSAGES.VALIDATION_FAILED, details: error.details});
+            return;
+        }
+
+        next();
+
+    } catch (error) {
+        res.status(500).json({success: "false", message: ERROR_MESSAGES.SERVER_ERROR, details: error});
+        return;
+    }
+}
